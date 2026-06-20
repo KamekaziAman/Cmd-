7bb6eb7620085c3de62f75535742d22248b33a32
Aman Rai <85382688+KamekaziAman@users.noreply.github.com>
2026-06-20 15:11:13 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || "https://cmdo-isj8.onrender.com/api/",
+export const api = axios.create({ "https://cmdo-isj8.onrender.com/api/commands",
 });
 
 api.interceptors.request.use((config) => {
